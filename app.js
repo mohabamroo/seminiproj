8b00a80b7d957ba8196a32dd464ea79b70226fd5
MOHAB <mohabamr1@gmail.com>
2017-03-05 09:48:17 +0200

    heroku

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use(function (req, res, next) {
 
 app.use('/', routes);
 app.use('/users', users);
-
-app.listen(3000, function() {
-	console.log("Express app started on port 3000.");
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), function() {
+  console.log("App started on port " + app.get("port"));
 }); 
